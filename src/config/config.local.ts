5e72361bdc657e507fb77bb82d927e899d70c151
吴占超 <zhanchao.wu@icloud.com>
2019-07-06 10:26:01 +0800

    jwt add

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -1,3 +1,5 @@
+import { Context } from 'midway';
+
 export const development = {
   watchDirs: [
     'app',
@@ -51,6 +53,36 @@ export const wxapi = {
   token: 'ujmik,123'
 };
 
+export const onerror = {
+  // all(err, ctx) {
+  //   // 在此处定义针对所有响应类型的错误处理方法
+  //   // 注意，定义了 config.all 之后，其他错误处理方法不会再生效
+  //   ctx.body = 'error';
+  //   ctx.status = 500;
+  // },
+  html(err: any, ctx: Context) {
+    // html hander
+    ctx.body = `<h3>${JSON.stringify(err)}</h3>`;
+    ctx.status = 500;
+  },
+  json(err: any, ctx: Context) {
+    // json hander
+    [422, 511, 512].includes(ctx.status) && (ctx.body = err);
+    // ctx.status = 511;
+  },
+  jsonp(err: any, ctx: Context) {
+    // 一般来说，不需要特殊针对 jsonp 进行错误定义，jsonp 的错误处理会自动调用 json 错误处理，并包装成 jsonp 的响应格式
+  }
+};
+
+/**
+ * jwt
+ */
+export const jwt = {
+  secret: 'gh_316a17f857f1',
+  expiresIn: '1 days'
+};
+
 export const cache = {
   default: 'memory',
   stores: {
@@ -58,6 +90,21 @@ export const cache = {
       driver: 'memory',
       max: 100,
       ttl: 0
+    }
+  }
+};
+
+export const joiSwagger = {
+  title: 'Api平台',
+  version: 'v1.0.0',
+  description: '开发环境文档',
+  swaggerOptions: {
+    securityDefinitions: {
+      apikey: {
+        type: 'apiKey',
+        name: 'auth',
+        in: 'header'
+      }
     }
   }
 };
